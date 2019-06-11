a7f423c0793b6eaaa14641f1221a75dcfba0f680
RadAd <adamgates84+github@gmail.com>
2019-06-11 15:06:38 +1000

    Update radgui and notepad

--- update.bat
+++ update.bat
@@ -1,9 +1,9 @@
 @echo off
 setlocal
 set CURL=curl.exe
-set JSON2XML=cscript /nologo "C:\Users\Adam.Gates\Desktop\json2xml.js"
+set JSON2XML=json2xml
 set XSLT=msxsl.exe
 
-%CURL% -ks -H "Accept: application/vnd.github.mercy-preview+json" "https://api.github.com/users/RadAd/repos?per_page=100&sort=name&direction=asc" > repos.json
-%JSON2XML% repos repos.json > repos.xml
-%XSLT% repos.xml repos-summary.xsl > repos-summary.html
+call %CURL% -ks -H "Accept: application/vnd.github.mercy-preview+json" "https://api.github.com/users/RadAd/repos?per_page=100&sort=name&direction=asc" > repos.json
+call %JSON2XML% repos repos.json > repos.xml
+call %XSLT% repos.xml repos-summary.xsl > repos-summary.html
